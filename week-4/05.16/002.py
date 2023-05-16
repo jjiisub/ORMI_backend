8211ae0e18a8668c760cb1d750b9c6148762f150
LeeJiSeop <jjiisub@naver.com>
2023-05-16 23:13:21 +0900

    05.16_4

--- week-4/05.16/002.py
+++ week-4/05.16/002.py
@@ -1,5 +1,6 @@
 # Advanced
 import re
+
 def md_to_html(data):
     # <h>
     data = re.sub(r'^# (.*)', r'<h1>\1</h1>', data, flags=re.M)
@@ -10,20 +11,54 @@ def md_to_html(data):
     # <em>
     data = re.sub(r'\*(.*)\*', r'<em>\1</em>', data)
     # <pre><code>
-    data = re.sub(r'\`\`\`(.*)\`\`\`', r'<pre><code>\1</code></pre>', data)
+    data = re.sub(r'(```)[\w]+', '<pre><code>', data)
+    data = re.sub(r'(```)', '</pre></code>', data)
     # <code>
     data = re.sub(r'\`(.*)\`', r'<code>\1</code>', data, flags=re.M)
     # <li>
+    data = insert_ul(data)
     data = re.sub(r'^- ([^\n]*)', r'<li>\1</li>', data, flags=re.M)
-    # <ol> 개선 필요!
-    data = re.sub(r'^1\.\s(.*)', r'<ol><li>\1</li>', data, flags=re.M)
-    data = re.sub(r'^2\.\s(.*)', r'<li>\1</li></ol>', data, flags=re.M)
+    # <ol>
+    data = insert_ol(data)
+    data = re.sub(r'\d+\.\s(.*)$', r'<li>\1</li>', data, flags=re.M)
     # <q>
     data = re.sub(r'^> (.*)', r'<q>\1</q>', data, flags=re.M)
     # <br>
     data = re.sub(r'(^---)', r'<br>', data, flags=re.M)
-
+    # <a>
+    data = re.sub(r'^\[(.+?)\]\((.+?)\)', r'<a href="\2">\1</a>', data, flags=re.M)
+    # <img>
+    data = re.sub(r'!\[(.+?)\]\((.+?)\)', r'<img href="\2">\1</img>', data, flags=re.M)
     return data
+
+def insert_ul(text):
+    pattern = r'^\-\s(.*)$'
+    matches = re.finditer(pattern, text, flags=re.M)
+    pos_min = float('inf')
+    pos_max = float('-inf')
+    for match in matches:
+        if match.span()[0] < pos_min:
+            pos_min = match.span()[0]
+        if match.span()[1] > pos_max:
+            pos_max = match.span()[1]
+    text = text[:pos_max]+'\n</ul>'+text[pos_max:]
+    text = text[:pos_min]+'<ul>\n'+text[pos_min:]
+    return text
+
+def insert_ol(text):
+    pattern = r'^(\d+)\. (.*)$'
+    matches = re.finditer(pattern, text, flags=re.M)
+    pos_min = float('inf')
+    pos_max = float('-inf')
+    for match in matches:
+        if match.span()[0] < pos_min:
+            pos_min = match.span()[0]
+        if match.span()[1] > pos_max:
+            pos_max = match.span()[1]
+    text = text[:pos_max]+'\n</ol>'+text[pos_max:]
+    text = text[:pos_min]+'<ol>\n'+text[pos_min:]
+    return text
+
 
 with open('sample.md', 'r') as ipf:
     text = ipf.read()
